7667dea867ed82f3ff694682e934b55249bb1f91
Adam Łangowski <adam.langowski@talkinthings.com>
2021-03-07 11:41:00 +0100

    Added fetch function + inpus.

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 
 export default class Table extends Component {
+
+    state = {}
+
+    componentDidMount() {
+        this.fetchUnits()
+    }
+
+    async fetchUnits() {
+        await fetch('',{})
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="row">
+                <div className="col-lg-2"></div>
+                <div className="col-lg-8">
+                    <div className="row">
+                        <div className="col-lg-3">
+                            <input name="food" type="text" className="input input-group"/>
+                        </div>
+                        <div className="col-lg-3">
+                            <input name="gold" type="text" className="input input-group"/>
+                        </div>
+                        <div className="col-lg-3">
+                            <input name="wood" type="text" className="input input-group"/>
+                        </div>
+                        <div className="col-lg-3">
+                            <input name="stone" type="text" className="input input-group"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-2"></div>
             </div>
         )
     }
