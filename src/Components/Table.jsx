d19a3aa08cbc553abc5554b8387caa593caa605a
Adam Łangowski <adam.langowski@talkinthings.com>
2021-03-07 20:42:21 +0100

    Added state amd fetch utl

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -2,7 +2,12 @@ import React, { Component } from 'react'
 
 export default class Table extends Component {
 
-    state = {}
+    state = {
+        food: 0,
+        gold: 0,
+        wood: 0,
+        stone: 0
+    }
 
     componentDidMount() {
         this.fetchUnits()
@@ -20,7 +25,8 @@ export default class Table extends Component {
     }
 
     async fetchUnits() {
-        await fetch('', {})
+        await fetch('https://age-of-empires-2-api.herokuapp.com/api/v1/units', {})
+            .then(x => x.json()).then(x => console.log(x))
     }
 
     render() {
@@ -30,16 +36,40 @@ export default class Table extends Component {
                 <div className="col-lg-8">
                     <div className="row">
                         <div className="col-lg-3">
-                            <input name="food" type="text" className="input input-group rounded" placeholder="Pozywienie" onChange={e => this.handleInputs(e)} />
+                            <input
+                                name="food"
+                                type="number"
+                                className="input input-group rounded"
+                                placeholder="Pozywienie"
+                                onChange={e => this.handleInputs(e)}
+                                value={this.state.food} />
                         </div>
                         <div className="col-lg-3">
-                            <input name="gold" type="text" className="input input-group rounded" placeholder="Złoto" onChange={e => this.handleInputs(e)} />
+                            <input
+                                name="gold"
+                                type="number"
+                                className="input input-group rounded"
+                                placeholder="Złoto"
+                                onChange={e => this.handleInputs(e)}
+                                value={this.state.gold} />
                         </div>
                         <div className="col-lg-3">
-                            <input name="wood" type="text" className="input input-group rounded" placeholder="Drewno" onChange={e => this.handleInputs(e)} />
+                            <input
+                                name="wood"
+                                type="number"
+                                className="input input-group rounded"
+                                placeholder="Drewno"
+                                onChange={e => this.handleInputs(e)}
+                                value={this.state.wood} />
                         </div>
                         <div className="col-lg-3">
-                            <input name="stone" type="text" className="input input-group rounded" placeholder="Kamień" onChange={e => this.handleInputs(e)} />
+                            <input
+                                name="stone"
+                                type="number"
+                                className="input input-group rounded"
+                                placeholder="Kamień"
+                                onChange={e => this.handleInputs(e)}
+                                value={this.state.stone} />
                         </div>
                     </div>
                     <div className="row my-4">
